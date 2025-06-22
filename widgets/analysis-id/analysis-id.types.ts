eceed5e12800e5857eaea99b3fabc13fc8f9ab88
Pavel <palllkaignatev@gmail.com>
2025-06-22 11:21:29 +0300

    Release

--- widgets/analysis-id/analysis-id.types.ts
+++ widgets/analysis-id/analysis-id.types.ts
@@ -1,5 +1,8 @@
+export type MessagePositionInGroup = 'single' | 'top' | 'middle' | 'bottom';
+
 export interface BaseMessage {
   id: number;
   text: string;
   fromId: string;
+  positionInGroup?: MessagePositionInGroup;
 }
